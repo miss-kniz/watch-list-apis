843af448c5164b8913ad0e6adc08386b8127c95b
Miss Kniz <mehak313naqvi@gmail.com>
2026-04-06 14:45:11 +0500

    Added register and login controller functions

---
+++ src/controllers/authController.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const register = async (req: express.Request, res: express.Response) => {};
+
+const login = async (req: express.Request, res: express.Response) => {};
+
+export { register, login };
