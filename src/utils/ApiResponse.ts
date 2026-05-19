95d55ecb65faed264b9ccdec3f173184ca8bd9fe
Miss Kniz <mehak313naqvi@gmail.com>
2026-05-19 16:01:19 +0500

    Add utility classes for standardized API responses, including success and error responses

---
+++ src/utils/ApiResponse.ts
@@ -0,0 +1,20 @@
+export class ApiError extends Error {
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class SuccessResponse<T = any> {
+  statusCode: number;
+  message: string;
+  data?: T;
+
+  constructor(statusCode: number, message: string, data?: T) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = data;
+  }
+}
